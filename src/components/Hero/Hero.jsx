40b8983e79cc6e6d1fe9915b9d87b0c6706f2dc6
Kononiaka Vladimir <kononiaka.vladimir@gmail.com>
2020-06-25 11:37:25 +0300

    navbar started

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Card from "./../UI/Card/Card";
+import Logo from "./../Logo/Logo";
+import Navbar from "./../Navbar/Navbar";
 
 const Hero = props => {
   return (
     <div>
-      <Card> Vals </Card>
+      <Card>
+        <div style={{ padding: "50px 0" }}>
+          <Logo />
+        </div>
+        <Navbar />
+      </Card>
     </div>
   );
 };
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Logo.css";
 
 const Logo = props => {
-  return <div className="logo">Vlad</div>;
+  return (
+    <div className="logo">
+      <a href="#"> Kononiaka</a>
+    </div>
+  );
 };
 
 export default Logo;
